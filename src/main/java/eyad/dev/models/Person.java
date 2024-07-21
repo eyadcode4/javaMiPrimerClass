2ffc12c164aa47172c97de3235f338c7ad10cf94
eyadcode24 <eyadcode24@gmail.com>
2024-07-21 21:32:38 +0200

    final good

--- src/main/java/eyad/dev/models/Person.java
+++ src/main/java/eyad/dev/models/Person.java
@@ -5,15 +5,15 @@ public class Person {
     private String name;
     private String lastName;
     private String dni;
-    private int birthDay;
+    private int birthYear;
     private String countryBirth;
     private char personGenere;
 
-    public Person(String name, String lastName, String dni, int birthDay, String countryBirth, char personGenere) {
+    public Person(String name, String lastName, String dni, int birthYear, String countryBirth, char personGenere) {
         this.name = name;
         this.lastName = lastName;
         this.dni = dni;
-        this.birthDay = birthDay;
+        this.birthYear = birthYear;
         this.countryBirth = countryBirth;
         this.personGenere = personGenere;
     }
@@ -30,7 +30,7 @@ public class Person {
     }
 
     public int getBirthDay() {
-        return birthDay;
+        return birthYear;
     }
 
     public String getCountryBirth() {
@@ -41,15 +41,13 @@ public class Person {
         return personGenere;
     }
 
-
-
     public void print() {
-        System.out.println("Person: " + name + "," + lastName);
-        
+        System.out.println("Name = " + name);
+        System.out.println("Last Name = " + lastName);
+        System.out.println("DNI = " + dni);
+        System.out.println("Birth Year = " + birthYear);
+        System.out.println("Birth Country = " + countryBirth);
+        System.out.println("Gender = " + personGenere);
     }
 
-
-
-   
-    
 }
